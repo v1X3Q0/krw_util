e990f0d1b2bb9949267ef38eaa267f5c2615bfc8
David Riusech <v1X3Q0@gmail.com>
2022-06-07 07:03:01 -0400

    accidentilly was incrememnting kernel pointers on access

--- krw_util/krw_ptr.h
+++ krw_util/krw_ptr.h
@@ -9,7 +9,76 @@
 #include <kern_dynamic.h>
 #include <krw_util.h>
 
-extern kern_dynamic* g_kernblock;
+extern kern_dynamic *g_kernblock;
+
+// MORTIMERE is when the struct has a size component, then we can add size operators
+#define MORTIMERE(KSTRUCT_NAME)                                            \
+    KSTRUCT_NAME operator[](const int location)                            \
+    {                                                                      \
+        size_t offset_local = 0;                                           \
+        g_kernblock->kstruct_offset(#KSTRUCT_NAME ".size", &offset_local); \
+        kern_address += location * offset_local;                           \
+        return *this;                                                      \
+    }                                                                      \
+    KSTRUCT_NAME &operator++()                                             \
+    {                                                                      \
+        size_t offset_local = 0;                                           \
+        g_kernblock->kstruct_offset(#KSTRUCT_NAME ".size", &offset_local); \
+        kern_address += offset_local;                                      \
+        return *this;                                                      \
+    }                                                                      \
+    KSTRUCT_NAME operator++(int)                                           \
+    {                                                                      \
+        KSTRUCT_NAME ptrBkp = *this;                                       \
+        size_t offset_local = 0;                                           \
+        g_kernblock->kstruct_offset(#KSTRUCT_NAME ".size", &offset_local); \
+        kern_address += offset_local;                                      \
+        return ptrBkp;                                                     \
+    }                                                                      \
+    KSTRUCT_NAME &operator--()                                             \
+    {                                                                      \
+        size_t offset_local = 0;                                           \
+        g_kernblock->kstruct_offset(#KSTRUCT_NAME ".size", &offset_local); \
+        kern_address -= offset_local;                                      \
+        return *this;                                                      \
+    }                                                                      \
+    KSTRUCT_NAME operator--(int)                                           \
+    {                                                                      \
+        KSTRUCT_NAME ptrBkp = *this;                                       \
+        size_t offset_local = 0;                                           \
+        g_kernblock->kstruct_offset(#KSTRUCT_NAME ".size", &offset_local); \
+        kern_address -= offset_local;                                      \
+        return ptrBkp;                                                     \
+    }
+
+// SABER is the constructor for each kern_struct inheritor
+#define SABER(KSTRUCT_NAME, ...)            \
+    class KSTRUCT_NAME : public kern_struct \
+    {                                       \
+        using kern_struct::kern_struct;     \
+                                            \
+    public:                                 \
+        __VA_ARGS__                         \
+    };
+
+// berserker is the template variant of saber
+#define BERSERKER(KSTRUCT_NAME, TYPE_NAME, ...) \
+    template <TYPE_NAME>                        \
+    SABER(KSTRUCT_NAME, __VA_ARGS__)
+
+// rider retrieves the kernel prim at the address
+#define RIDER(RET_TYPE, OFFSET_BASE, MEM_NAME) \
+    RET_TYPE MEM_NAME() { return kread_member<RET_TYPE>(#OFFSET_BASE "." #MEM_NAME); };
+
+// lancer is used as a getter for the offset of a type
+#define LANCER(RET_TYPE, OFFSET_BASE, MEM_NAME) \
+    RET_TYPE MEM_NAME() { return kadd_member<RET_TYPE>(#OFFSET_BASE "." #MEM_NAME); };
+
+#define JEAN(RET_TYPE, OFFSET_BASE, MEM_NAME) \
+    RET_TYPE MEM_NAME() { return kadd_member_save<RET_TYPE>(#OFFSET_BASE "." #MEM_NAME); };
+
+#define ASTOLPHO(RET_TYPE, OFFSET_BASE, MEM_NAME) \
+    RET_TYPE MEM_NAME() { return kread_member_save<RET_TYPE>(#OFFSET_BASE "." #MEM_NAME); };
 
 // to be used for kernel pointers of primitive types
 template <typename t>
@@ -17,12 +86,13 @@ class kern_ptr
 {
 private:
     size_t kern_address;
+
 public:
-    kern_ptr(size_t kernel_address_a) : kern_address(kernel_address_a) {};
-//    Point& operator++();       // Prefix increment operator.
-//    Point operator++(int);     // Postfix increment operator.
+    kern_ptr(size_t kernel_address_a) : kern_address(kernel_address_a){};
+    //    Point& operator++();       // Prefix increment operator.
+    //    Point operator++(int);     // Postfix increment operator.
     size_t get_kaddr() { return kern_address; };
-    kern_ptr& operator++()
+    kern_ptr &operator++()
     {
         kern_address += sizeof(t);
         return *this;
@@ -35,7 +105,7 @@ public:
         return ptrBkp;
     }
 
-    kern_ptr& operator--()
+    kern_ptr &operator--()
     {
         kern_address -= sizeof(t);
         return *this;
@@ -55,6 +125,11 @@ public:
         return tempObj;
     }
 
+    size_t operator!() const
+    {
+        return get_kaddr();
+    }
+
     t operator=(t other)
     {
         kWrite(&other, sizeof(t), kern_address);
@@ -62,40 +137,84 @@ public:
     }
 };
 
+template <typename bp_class>
+class kern_bkptr
+{
+protected:
+    size_t kern_address;
+    size_t off_save;
+public:
+    kern_bkptr(size_t new_address, size_t off_save_a) : kern_address(new_address), off_save(off_save_a){};
+
+    bp_class operator*() const
+    {
+        return bp_class(kern_address - off_save);
+    }
+
+    size_t operator!() const
+    {
+        return kern_address;
+    }
+};
 
 class kern_struct
 {
 protected:
     size_t kern_address;
+    size_t off_save;
     std::string class_name;
+
 public:
-    kern_struct(size_t new_address) : kern_address(new_address) {};
-    
+    kern_struct(size_t new_address) : kern_address(new_address), off_save(0){};
+    kern_struct(size_t new_address, size_t off_save_a) : kern_address(new_address), off_save(off_save_a){};
+
     // read a member whose offset is specified by key member_key
-    template<typename t>
+    template <typename t>
     t kread_member(std::string member_key)
     {
         size_t offset_local = 0;
         size_t new_kern_addr = 0;
 
         SAFE_PEXIT(g_kernblock->kstruct_offset(member_key, &offset_local) == -1, "symerror on symbol %s\n", member_key.data());
-        
+
         kReadPtr(&new_kern_addr, sizeof(new_kern_addr), kern_address + offset_local);
         return t(new_kern_addr);
     }
-    
-    template<typename t>
+
+    template <typename t>
+    t kread_member_save(std::string member_key)
+    {
+        size_t offset_local = 0;
+        size_t new_kern_addr = 0;
+
+        SAFE_PEXIT(g_kernblock->kstruct_offset(member_key, &offset_local) == -1, "symerror on symbol %s\n", member_key.data());
+
+        kReadPtr(&new_kern_addr, sizeof(new_kern_addr), kern_address + offset_local);
+        return t(new_kern_addr, offset_local + off_save);
+    }
+
+    template <typename t>
     t kadd_member(std::string member_key)
     {
         size_t offset_local = 0;
         size_t new_kern_addr = 0;
-        
+
         SAFE_PEXIT(g_kernblock->kstruct_offset(member_key, &offset_local) == -1, "symerror on symbol %s\n", member_key.data());
 
-        kern_address += offset_local;
-        return t(kern_address);
+        return t(kern_address + offset_local);
     }
-    
+
+    template <typename t>
+    t kadd_member_save(std::string member_key)
+    {
+        size_t offset_local = 0;
+        size_t new_kern_addr = 0;
+
+        SAFE_PEXIT(g_kernblock->kstruct_offset(member_key, &offset_local) == -1, "symerror on symbol %s\n", member_key.data());
+
+        return t(kern_address + offset_local, offset_local + off_save);
+    }
+
     size_t operator*() const
     {
         return kern_address;
@@ -112,69 +231,9 @@ public:
     }
 };
 
-// MORTIMERE is when the struct has a size component, then we can add size operators
-#define MORTIMERE(KSTRUCT_NAME) \
-    KSTRUCT_NAME operator[](const int location) \
-    { \
-        size_t offset_local = 0; \
-        g_kernblock->kstruct_offset(# KSTRUCT_NAME ".size", &offset_local); \
-        kern_address += location * offset_local; \
-        return *this; \
-    } \
-    KSTRUCT_NAME& operator++() \
-    { \
-        size_t offset_local = 0; \
-        g_kernblock->kstruct_offset(# KSTRUCT_NAME ".size", &offset_local); \
-        kern_address += offset_local; \
-        return *this; \
-    } \
-    KSTRUCT_NAME operator++(int) \
-    { \
-        KSTRUCT_NAME ptrBkp = *this; \
-        size_t offset_local = 0; \
-        g_kernblock->kstruct_offset(# KSTRUCT_NAME ".size", &offset_local); \
-        kern_address += offset_local; \
-        return ptrBkp; \
-    } \
-    KSTRUCT_NAME& operator--() \
-    { \
-        size_t offset_local = 0; \
-        g_kernblock->kstruct_offset(# KSTRUCT_NAME ".size", &offset_local); \
-        kern_address -= offset_local; \
-        return *this; \
-    } \
-    KSTRUCT_NAME operator--(int) \
-    { \
-        KSTRUCT_NAME ptrBkp = *this; \
-        size_t offset_local = 0; \
-        g_kernblock->kstruct_offset(# KSTRUCT_NAME ".size", &offset_local); \
-        kern_address -= offset_local; \
-        return ptrBkp; \
-    }
-
-// SABER is the constructor for each kern_struct inheritor
-#define SABER(KSTRUCT_NAME, ...) \
-class KSTRUCT_NAME : public kern_struct \
-{ \
-    using kern_struct::kern_struct; \
-public: \
-    __VA_ARGS__ \
-};
-
-// berserker is the template variant of saber
-#define BERSERKER(KSTRUCT_NAME, TYPE_NAME, ...) \
-    template<TYPE_NAME> \
-    SABER(KSTRUCT_NAME, __VA_ARGS__)
-
-// rider retrieves the kernel prim at the address
-#define RIDER(RET_TYPE, OFFSET_BASE, MEM_NAME) \
-        RET_TYPE MEM_NAME() { return kread_member<RET_TYPE>( # OFFSET_BASE "." # MEM_NAME); };
-
-// lancer is used as a getter for the offset of a type
-#define LANCER(RET_TYPE, OFFSET_BASE, MEM_NAME) \
-        RET_TYPE MEM_NAME() { return kadd_member<RET_TYPE>( # OFFSET_BASE "." # MEM_NAME); };
-
 // generic definitions for all operating systems
-BERSERKER(list_entry, typename T, RIDER(T, list_entry<T>, next) RIDER(T, list_entry<T>, prev));
+BERSERKER(list_entry, typename T,
+    ASTOLPHO(T, list_entry<T>, next)
+    ASTOLPHO(T, list_entry<T>, prev));
 
 #endif
--- krw_util/krw_util.cpp
+++ krw_util/krw_util.cpp
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#ifdef __APPLE__
-#include <TargetConditionals.h>
-#endif
-
 #include <localUtil.h>
 #include <kernel_block.h>
 
@@ -12,89 +8,106 @@
 #include <kern_dynamic.h>
 #include <kern_static.h>
 
+#ifdef __APPLE__
+#include <TargetConditionals.h>
+#elif defined _WIN32
+#include <Windows.h>
+#include <ntkern_tools.h>
+#endif
+
 #include "krw_util.h"
 
-kern_dynamic *g_kernblock = 0;
+kern_dynamic* g_kernblock = 0;
 
 int kInit()
 {
-    int result = -1;
-    size_t found_base = 0;
+	int result = -1;
+	size_t found_base = 0;
+    kern_static* kern_tmp = 0;
 
-    SAFE_BAIL(kernel_init() == -1);
-    SAFE_BAIL(kernel_base(&found_base) == -1);
+	SAFE_BAIL(kernel_init() == -1);
+#ifdef _WIN32
+    kern_tmp = kernel_block::allocate_kern_img<kern_static>(KERNEL_PATH);
+	SAFE_BAIL(kernel_base(kern_tmp, &found_base) == -1);
+#else
+	SAFE_BAIL(kernel_base(&found_base)== -1);
+#endif
+    
+#ifdef _WIN32
+	g_kernblock = kernel_block::grab_live_kernel<kern_dynamic>((void*)found_base, (kernel_block*)kern_tmp);
+#else
+	g_kernblock = kernel_block::grab_live_kernel<kern_dynamic>((void*)found_base);
+#endif
+	SAFE_BAIL(g_kernblock == 0);
 
-     g_kernblock = kernel_block::grab_live_kernel<kern_dynamic>((void *)found_base);
-     SAFE_BAIL(g_kernblock == 0);
-
-    result = 0;
+	result = 0;
 fail:
-    return result;
+	return result;
 }
 
-int kRead(void *buf, size_t len, size_t offset)
+int kRead(void* buf, size_t len, size_t offset)
 {
-    return kernel_read(buf, len, offset);
+	return kernel_read(buf, len, offset);
 }
 
-int kReadPtr(void *buf, size_t len, size_t offset)
+int kReadPtr(void* buf, size_t len, size_t offset)
 {
-    return kernel_read_ptr(buf, len, offset);
+	return kernel_read_ptr(buf, len, offset);
 }
 
-int kWrite(void *buf, size_t len, size_t offset)
+int kWrite(void* buf, size_t len, size_t offset)
 {
-    return kernel_write(buf, len, offset);
+	return kernel_write(buf, len, offset);
 }
 
-int kWritePtr(void *buf, size_t len, size_t offset)
+int kWritePtr(void* buf, size_t len, size_t offset)
 {
-    return kernel_write_ptr(buf, len, offset);
+	return kernel_write_ptr(buf, len, offset);
 }
 
-int kSlide(size_t *a_kernelSlide)
+int kSlide(size_t* a_kernelSlide)
 {
-    return kernel_slide(a_kernelSlide);
+	return kernel_slide(a_kernelSlide);
 }
 
-int kBase(size_t *a_kernelBase)
+int kBase(size_t* a_kernelBase)
 {
-    return kernel_base(a_kernelBase);
+	return kernel_base(a_kernelBase);
 }
 
 int kClose()
 {
-    return kernel_close();
+	return kernel_close();
 }
 
 size_t kSlideTarg(size_t targ)
 {
-    size_t resultTarg = -1;
-    size_t kernSlideTmp = 0;
-    SAFE_BAIL(kernel_slide(&kernSlideTmp) == -1);
+	size_t resultTarg = -1;
+	size_t kernSlideTmp = 0;
+	SAFE_BAIL(kernel_slide(&kernSlideTmp) == -1);
 
-    resultTarg = targ + kernSlideTmp;
+	resultTarg = targ + kernSlideTmp;
 fail:
-    return resultTarg;
+	return resultTarg;
 }
 
 size_t kBaseTarg(size_t targ)
 {
-    size_t resultTarg = -1;
-    size_t kernBaseTmp = 0;
-    SAFE_BAIL(kernel_base(&kernBaseTmp) == -1);
+	size_t resultTarg = -1;
+	size_t kernBaseTmp = 0;
+	SAFE_BAIL(kernel_base(&kernBaseTmp) == -1);
 
-    resultTarg = targ + kernBaseTmp;
+	resultTarg = targ + kernBaseTmp;
 fail:
-    return resultTarg;
+	return resultTarg;
 }
 
-void *kdlsym(const char *symbol)
+void* kdlsym(const char* symbol)
 {
-    void *result = 0;
+	void* result = 0;
 
-    FINISH_IF(g_kernblock->ksym_dlsym(symbol, (size_t *)&result) == -1);
+	FINISH_IF(g_kernblock->ksym_dlsym(symbol, (size_t*)&result) == -1);
 
 finish:
-    return result;
+	return result;
 }
