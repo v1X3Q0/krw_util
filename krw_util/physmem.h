f48ef101f35154b56cd2c24671eead16d35c2713
David Riusech <david.riusech@blackhorsesolutions.com>
2022-08-17 04:25:12 -0400

    metalkit used a different variant of the kvtop

--- krw_util/physmem.h
+++ krw_util/physmem.h
@@ -12,6 +12,12 @@ size_t phystovirt(size_t arg1);
 }
 #endif
 
+#ifdef __METALKIT__
+#ifdef __cplusplus
+extern "C"
+#endif // __cplusplus
+#endif // __METALKIT__
+// metalkit seems to need this definition?
 int kvtop(size_t kv, size_t* phys_addr);
 
 #endif // PHYSMEM_H
