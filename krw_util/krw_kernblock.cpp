c9bab8aa799f6957074946ffc6e7ad5a95c10165
David Riusech <v1X3Q0@gmail.com>
2022-09-30 05:37:36 -0400

    pulled out kernblock stuff, separated each one so that they can be indexed individuallly

--- krw_util/krw_kernblock.cpp
+++ krw_util/krw_kernblock.cpp
@@ -27,35 +27,89 @@ kern_dynamic* g_kernblock = 0;
 
 #include "krw_util.h"
 
+// mac is the most straightforward, with the driver framework and the
+// ability to read the kernel as a file, and base it using the mach
+// header.
 int kInit_notsimple()
+#ifdef __APPLE__
 {
 	int result = -1;
 	size_t found_base = 0;
-#ifndef __linux__
     kern_static* kern_tmp = 0;
-#endif
 
 	SAFE_BAIL(kernel_init() == -1);
-#ifdef _WIN32
-    kern_tmp = kernel_block::allocate_kern_img<kern_static>(KERNEL_PATH);
-	SAFE_BAIL(kernel_base_block(kern_tmp, &found_base) == -1);
-#elif defined(__APPLE__) || (defined(__linux__) && !defined(__METALKIT__))
 	SAFE_BAIL(kernel_base(&found_base)== -1);
-#endif
     
-#ifdef _WIN32
-	g_kernblock = kernel_block::grab_live_kernel<kern_dynamic>((void*)found_base, (kernel_block*)kern_tmp);
-#elif defined(__APPLE__) || (defined(__linux__) && !defined(__METALKIT__))
 	g_kernblock = kernel_block::grab_live_kernel<kern_dynamic>((void*)found_base);
-#elif defined(__METALKIT__)
-	g_kernblock = new kernel_metalkit((uint32_t*)found_base);
-#endif
 	SAFE_BAIL(g_kernblock == 0);
 
 	result = 0;
 fail:
 	return result;
 }
+// windows also has the kernel accessible from the filesystem, so once
+// we have a kernel read write and leak, we can then load the kernel as
+// a file and then use that to guess where the kernel base is.
+#elif defined(_WIN32)
+{
+	int result = -1;
+	size_t found_base = 0;
+    kern_static* kern_tmp = 0;
+
+	SAFE_BAIL(kernel_init() == -1);
+    kern_tmp = kernel_block::allocate_kern_img<kern_static>(KERNEL_PATH);
+	SAFE_BAIL(kernel_base_block(kern_tmp, &found_base) == -1);
+    
+	g_kernblock = kernel_block::grab_live_kernel<kern_dynamic>((void*)found_base, (kernel_block*)kern_tmp);
+	SAFE_BAIL(g_kernblock == 0);
+
+	result = 0;
+fail:
+	return result;
+}
+// metalkit doesn't need a base, so perform a kernel init then get
+// straight to it.
+#elif defined(__METALKIT__)
+{
+	int result = -1;
+	size_t found_base = 0;
+
+	SAFE_BAIL(kernel_init() == -1);
+    
+	g_kernblock = new kernel_metalkit((uint32_t*)found_base);
+	SAFE_BAIL(g_kernblock == 0);
+
+	result = 0;
+fail:
+	return result;
+}
+// for linux, cause there are so many variants and a difference between
+// root and not, the root population and internal population are separate.
+// all things considered, populate with the devils you know, then with
+// the devils you don't.
+#elif defined(__linux__)
+{
+	int result = -1;
+	size_t found_base = 0;
+
+
+	SAFE_BAIL(kernel_init() == -1);
+
+	SAFE_BAIL(kernel_base(&found_base)== -1);
+	
+	if (g_kernblock == 0)
+	{
+		g_kernblock = kernel_block::grab_live_kernel<kern_dynamic>((void*)found_base);
+	}
+	
+    
+	SAFE_BAIL(g_kernblock == 0);
+
+	result = 0;
+fail:
+	return result;
+}
+#endif
 
 int kInit_simple()
 {
